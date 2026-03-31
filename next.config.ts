df3664c88cc863c7364a51106c6672466244031c
Tyowk <tyowk.personal@gmail.com>
2026-04-01 02:47:39 +0700

    Add image domains to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: [
+      "s1.imgcdn.dev"
+      "s2.imgcdn.dev"
+      "s3.imgcdn.dev"
+      "s4.imgcdn.dev"
+      "s5.imgcdn.dev"
+      "s6.imgcdn.dev",
+      "imgcdn.dev"
+    ],
     remotePatterns: [
       {
         protocol: "https",
