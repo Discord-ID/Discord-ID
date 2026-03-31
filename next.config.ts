54723c092a1d01bc9369df57213de62fa0c72def
Tyowk <tyowk.personal@gmail.com>
2026-04-01 02:48:05 +0700

    lupa comma hehe

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      "s1.imgcdn.dev"
-      "s2.imgcdn.dev"
-      "s3.imgcdn.dev"
-      "s4.imgcdn.dev"
-      "s5.imgcdn.dev"
+      "s1.imgcdn.dev",
+      "s2.imgcdn.dev",
+      "s3.imgcdn.dev",
+      "s4.imgcdn.dev",
+      "s5.imgcdn.dev",
       "s6.imgcdn.dev",
       "imgcdn.dev"
     ],
